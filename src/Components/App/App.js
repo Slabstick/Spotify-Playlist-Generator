516b7b39a08fd43edc3913c3cc484711339b7a55
Slabstick <slabsticktwitch@gmail.com>
2022-05-13 23:32:53 +0200

    first working version

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,27 +3,70 @@ import React from 'react';
 import {SearchBar} from '../SearchBar/SearchBar'
 import {SearchResults} from '../SearchResults/SearchResults'
 import {Playlist} from '../Playlist/Playlist'
+import Spotify from '../../util/Spotify';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
     this.state = {
-      searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, {name: 'name3', artist: 'artist3', album: 'album3', id: 3}]
+      searchResults: [],
+      playlistName: 'New Playlist',
+      playlistTracks: [],
     }
+  }
+  addTrack(track){
+    let tracks = this.state.playlistTracks;
+    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+    tracks.push(track);
+    this.setState({playlistTracks : tracks});
+  }
+  removeTrack(track){
+    let tracks = this.state.playlistTracks.filter(toRemove => toRemove.id !== track.id);
+    this.setState({playlistTracks: tracks});    
+  }
+  updatePlaylistName(name) {
+    this.setState({playlistName: name});
+  }
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+      this.setState({
+        playlistName: 'New Playlist',
+        playlistTracks: []
+      });
+    });
+  }
+  search(term) {    
+    Spotify.search(term).then(searchResults => {
+      this.setState({searchResults: searchResults})
+    })
   }
   render() {
     return (
       <div>
-        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <h1>SP<span className="highlight">oOo</span>DLE</h1>
         <div className="App">
-          {/* Add a SearchBar component */}
-          <SearchBar />
+          
+          {/* SearchBar component */}
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
-            {/* Add a SearchResults component */}
-            <SearchResults searchResults={this.state.searchResults} />
-            {/* Add a Playlist component */}
-            <Playlist />
+            {/* SearchResults component */}
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            {/* Playlist component */}
+            <Playlist 
+              playlistName={this.state.playlistName} 
+              playlistTracks={this.state.playlistTracks} 
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist} />
           </div>
         </div>
       </div>);
